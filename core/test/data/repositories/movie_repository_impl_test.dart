8442c11e11355d60bf7e4b08bde9c6f7a320e36a
ahmadsufyan455 <ahmadsufyan455@gmail.com>
2023-12-17 00:26:56 +0700

    modify test data

--- core/test/data/repositories/movie_repository_impl_test.dart
+++ core/test/data/repositories/movie_repository_impl_test.dart
@@ -15,7 +15,7 @@ import 'package:core/domain/entities/tv.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
-import '../../dummy_data/dummy_objects.dart';
+import '../../../../test/dummy_data/dummy_objects.dart';
 import '../../helpers/test_helper.mocks.dart';
 
 void main() {
---
+++ test/dummy_data/dummy_objects.dart
@@ -0,0 +1,142 @@
+import 'package:core/data/models/movie_table.dart';
+import 'package:core/data/models/tv_table.dart';
+import 'package:core/domain/entities/genre.dart';
+import 'package:core/domain/entities/movie.dart';
+import 'package:core/domain/entities/movie_detail.dart';
+import 'package:core/domain/entities/tv.dart';
+import 'package:core/domain/entities/tv_detail.dart';
+
+const testMovie = Movie(
+  adult: false,
+  backdropPath: '/muth4OYamXf41G2evdrLEg8d3om.jpg',
+  genreIds: [14, 28],
+  id: 557,
+  originalTitle: 'Spider-Man',
+  overview:
+      'After being bitten by a genetically altered spider, nerdy high school student Peter Parker is endowed with amazing powers to become the Amazing superhero known as Spider-Man.',
+  popularity: 60.441,
+  posterPath: '/rweIrveL43TaxUN0akQEaAXL6x0.jpg',
+  releaseDate: '2002-05-01',
+  title: 'Spider-Man',
+  video: false,
+  voteAverage: 7.2,
+  voteCount: 13507,
+);
+
+final testMovieList = [testMovie];
+
+const testMovieDetail = MovieDetail(
+  adult: false,
+  backdropPath: 'backdropPath',
+  genres: [Genre(id: 1, name: 'Action')],
+  id: 1,
+  originalTitle: 'originalTitle',
+  overview: 'overview',
+  posterPath: 'posterPath',
+  releaseDate: 'releaseDate',
+  runtime: 120,
+  title: 'title',
+  voteAverage: 1,
+  voteCount: 1,
+);
+
+const testTv = Tv(
+  backdropPath: '/muth4OYamXf41G2evdrLEg8d3om.jpg',
+  genreIds: [14, 28],
+  id: 557,
+  originalName: 'Spider-Man',
+  overview:
+      'After being bitten by a genetically altered spider, nerdy high school student Peter Parker is endowed with amazing powers to become the Amazing superhero known as Spider-Man.',
+  popularity: 60.441,
+  posterPath: '/rweIrveL43TaxUN0akQEaAXL6x0.jpg',
+  firstAirDate: '2002-05-01',
+  name: 'Spider-Man',
+  voteAverage: 7.2,
+  voteCount: 13507,
+  originCountry: ['en'],
+  originalLanguage: 'en',
+);
+
+final testTvList = [testTv];
+
+final testTvDetail = TvDetail(
+  backdropPath: 'backdropPath',
+  genres: const [Genre(id: 1, name: 'Action')],
+  id: 1,
+  originalName: 'originalName',
+  overview: 'overview',
+  posterPath: 'posterPath',
+  firstAirDate: 'firstAirDate',
+  lastAirDate: 'lastAirDate',
+  name: 'name',
+  voteAverage: 1,
+  voteCount: 1,
+  episodeRunTime: const [1],
+  homepage: 'homePage',
+  inProduction: true,
+  languages: const ['en'],
+  numberOfEpisodes: 1,
+  numberOfSeasons: 1,
+  originCountry: const ['USA'],
+  originalLanguage: 'en',
+  popularity: 1,
+  seasons: seasons,
+  status: 'status',
+  tagline: 'tagLine',
+  type: 'type',
+);
+
+final List<Season> seasons = [
+  const Season(
+    airDate: 'airDate',
+    episodeCount: 1,
+    id: 1,
+    name: 'name',
+    overview: 'overview',
+    posterPath: 'posterPath',
+    seasonNumber: 1,
+    voteAverage: 1,
+  )
+];
+
+const testWatchlistMovie = Movie.watchlist(
+  id: 1,
+  title: 'title',
+  posterPath: 'posterPath',
+  overview: 'overview',
+);
+
+const testWatchlistTv = Tv.watchlist(
+  id: 1,
+  name: 'name',
+  posterPath: 'posterPath',
+  overview: 'overview',
+);
+
+const testMovieTable = MovieTable(
+  id: 1,
+  title: 'title',
+  posterPath: 'posterPath',
+  overview: 'overview',
+);
+
+const testTvTable = TvTable(
+  id: 1,
+  name: 'name',
+  posterPath: 'posterPath',
+  overview: 'overview',
+);
+
+final testMovieMap = {
+  'id': 1,
+  'overview': 'overview',
+  'posterPath': 'posterPath',
+  'title': 'title',
+};
+
+final testTvMap = {
+  'id': 1,
+  'overview': 'overview',
+  'posterPath': 'posterPath',
+  'name': 'name',
+};
